c2499dea3e64929889bb9d7e6759c254a6667372
pasus <145437900+pasus-mern@users.noreply.github.com>
2023-09-28 16:29:45 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,12 +1,26 @@
+//question 1
+//How to compare two JSON have the same properties without order
 // comparing two Json have the same properties without order.
 
 let obj1 = {name:"person1", age:5};
 let obj2 = {age:5, name:"person1"};
 
+var assumption=true;
+
 if(Object.keys(obj1).length==Object.keys(obj2).length){
-  console.log(true);
+    for(let key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            assumption=false;
+            break;
+        }
+    }
 }
-else
-{
-  console.log(false);
+else {
+    assumption=false;
 }
+console.log(assumption);
+//output:
+//true
